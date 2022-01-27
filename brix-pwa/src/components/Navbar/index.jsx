5e72b2e06f8aa23724aadeec67ff35c5c3b301c8
laurens <voogdlaurens@gmail.com>
2022-01-27 15:36:33 +0100

    fixed dev deploy

--- brix-pwa/src/components/Navbar/index.jsx
+++ brix-pwa/src/components/Navbar/index.jsx
@@ -4,9 +4,9 @@ import classNames from "classnames";
 
 const Navbar = ({ navigationData, currentRoute, setCurrentRoute }) => {
   return (
-    <nav className='hidden md:flex m-3 mb-30 bg-white shadow-md'>
+    <nav className='hidden md:flex m-3  bg-white shadow-md'>
       <div className='pl-4 pr-10'>
-        <a href='#' className='flex items-center'>
+        <a href='/#' className='flex items-center'>
           <img
             className='w-10 h-10'
             src='https://cdn3.iconfinder.com/data/icons/random-4/96/brick-bricks-build-building-contruction-wall-512.png'
@@ -15,14 +15,14 @@ const Navbar = ({ navigationData, currentRoute, setCurrentRoute }) => {
           <span className='font-semibold text-gray-500 text-2xl'> Brix </span>
         </a>
       </div>
-      <div className='flex-1 flex flex-row justify-start pl-50'>
+      <div className='flex-1 flex flex-row justify-start pl-50 '>
         {navigationData.map((item, index) => (
           <a
-            href='#'
+            href='/#'
             className={classNames([
-              "w-22 text-gray-500 text-xl hover:text-red-400 hover:border-red-400 hover:border-b-4 font-semibold cursor-pointer font-large tracking-wide text-sm flex items-start justify-center py-2 px-4",
+              "w-22 text-gray-500 text-xl hover:text-red-400 hover:border-red-400 hover:border-b-4 font-semibold cursor-pointer font-large tracking-wide text-sm flex items-start justify-center py-2 px-4 pb-5" ,
               currentRoute === item &&
-                "text-gray-700 border-b-3 border-gray-700 bg-gradient-to-b from-white to-gray-100 border-red-400 border-b-4",
+                "text-gray-700 border-b-3 border-gray-700 bg-gradient-to-b from-white to-gray-100 border-red-400 border-b-4 ",
             ])}
             key={index}
             onClick={() => setCurrentRoute(item)}>

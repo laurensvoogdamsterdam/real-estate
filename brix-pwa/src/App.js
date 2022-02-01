c1e7527a56013132a27f757d0adf110c7f9f645e
laurens <voogdlaurens@gmail.com>
2022-02-01 08:26:52 +0100

    Removed all unused imports

--- brix-pwa/src/App.js
+++ brix-pwa/src/App.js
@@ -16,22 +16,22 @@ import Discover from './screens/Discover'
 
 export default function App() {
   const { currentRoute, setCurrentRoute } = useNavigation();
-  const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [term, setTerm] = useState("");
+  // const [images, setImages] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [term, setTerm] = useState("");
   
 
-  useEffect(() => {
-    fetch(
-      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setImages(data.hits);
-        setIsLoading(false);
-      })
-      .catch((err) => console.log(err));
-  }, [term]);
+  // useEffect(() => {
+  //   fetch(
+  //     `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setImages(data.hits);
+  //       setIsLoading(false);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, [term]);
 
   return (          
     <Router>

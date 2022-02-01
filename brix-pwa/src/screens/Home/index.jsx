9c332cc5c7920880df81f859af771dc87198f866
laurens <voogdlaurens@gmail.com>
2022-02-01 14:50:47 +0100

    Second version

--- brix-pwa/src/screens/Home/index.jsx
+++ brix-pwa/src/screens/Home/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import Landing from '../Landing';
-import Cards from '../../components/Cards';
 import Hero from '../../components/Hero';
+
 function Home() {
-    return (
-        // <Landing heading="Brix">
-        //     <Cards />
-        // </Landing>
+    return (        
         <Hero />
     )
 }
